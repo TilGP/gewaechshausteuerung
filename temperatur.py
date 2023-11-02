ae7d19378aed5aeefb6216d2cdf731c1416e6ec6
til.pockrandt <til.pockrandt@check24.de>
2023-11-02 11:04:11 +0100

    Kommentare üersetzt

--- temperatur.py
+++ temperatur.py
@@ -7,11 +7,11 @@ import board
 import RPi.GPIO as GPIO
 import dht11
 
-#from Adafruit_LED_Backpack import SevenSegment
+# aus dem Adafruit_LED_Backpack die 7-Segment-Display Klasse importieren.
 from adafruit_ht16k33.segments import Seg7x4
 
 i2c = board.I2C()
-segment = Seg7x4(i2c, address=0x70) #segment der I2C Adresse 0x70 und die Displaydefinition zuweisen
+segment = Seg7x4(i2c, address=0x70) # segment der I2C Adresse 0x70 und die Displaydefinition zuweisen
 
 segment.fill(0) # Initialisierung des Displays. Muss einmal ausgeführt werden bevor das Display benutzt wird.
 
@@ -19,17 +19,17 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# read data using pin 14
+# Daten über den Pin 14 auslehsen
 instance = dht11.DHT11(pin = 4)
 
-print ("STRG+C Druecken zum beenden.") #print Befehl für Ausgabe zum beenden des Scriptes
+print("STRG+C Druecken zum beenden.") # print Befehl für Ausgabe zum beenden des Scriptes
 
-#Schleife welche dauerhaft die Zeit updated und sie auf dem Display anzeigt.
+# Schleife, die durchgängig die Temperatur misst und diese auf dem Segment-Display anzeigt
 try:
   while(True):
     
     result = instance.read()
-    while not result.is_valid(): # read until valid values
+    while not result.is_valid(): # lesen, bis das Ergebnis valide ist 
         result = instance.read()
     print(result.temperature)
 
@@ -43,6 +43,6 @@ try:
 
     segment.show() # Wird benötigt um die Display LEDs zu updaten.
 
-    time.sleep(20) # Warte eine Sekunde
-except KeyboardInterrupt:
+    time.sleep(20) # Warte zwei Sekunden
+except KeyboardInterrupt: Fange ein STRG+C ab und schalte das Display aus
     segment.fill(0)
