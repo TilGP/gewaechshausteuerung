3a60628886a800fd7ac798666f771e06467648bd
til.pockrandt <til.pockrandt@check24.de>
2024-03-07 10:06:45 +0100

    fix

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
 from luma.core.legacy import text
+from luma.core.legacy.font import proportional, CP437_FONT
 
 serial = spi(port=0, device=1, gpio=noop())
 device = max7219(serial, cascaded=1, block_orientation=90, rotate=0)
