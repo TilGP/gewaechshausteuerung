a340829efa3474053a39571c8c46435be837d6b6
til.pockrandt <til.pockrandt@check24.de>
2024-05-23 10:26:12 +0200

    changes

--- relay.py
+++ relay.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 # definiere Relay Pin
-relay_pin = 40
+relay_pin = 21
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(relay_pin, GPIO.OUT)
 
-GPIO.output(relay_pin, GPIO.LOW)
+GPIO.output(relay_pin, GPIO.LOW)  # an
 time.sleep(0.5)
-GPIO.output(relay_pin, GPIO.HIGH)
+GPIO.output(relay_pin, GPIO.HIGH)  # aus
 GPIO.cleanup()
